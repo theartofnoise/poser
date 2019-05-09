2273c562cf99e4f12edda85e6343c4b0ce1b3b20
Art G <theartofnoise@gmail.com>
2019-05-08 23:35:06 -0400

    test

--- client/src/components/AudioBtn/index.js
+++ client/src/components/AudioBtn/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css"
 
+// audio button
 function AudioBtn(props) {
   return (
     <button onClick={props.onClick} className="btn btn-success" role="button" tabIndex="0">
