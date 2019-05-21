05206f080dd3fc3cf95c94be58c041bf5536f914
Art G <theartofnoise@gmail.com>
2019-05-21 14:43:57 -0400

    feat: added delete button functionality

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBContainer, MDBBtn } from "mdbreact";
+import { Link, withRouter} from "react-router-dom";
+import API from '../../utils/API'
 
 
-// This file exports both the List and ListItem components
+// This file exports both project components
+
+
+// const deleteBtn = (id)=> {
+//   API.deleteLyric(id)
+//     .then(res =>{ console.log(res)})
+//     .catch(err => console.log(err))
+// }
 
 function Projects(props) {
 
   return (
-    // <div onClick={props.onClick} className="btn btn-success" role="button" tabIndex="0">
-    //   <br/>
-    //   title: {props.title} <br/>
-    //   by: {props.author} <br/>
-      
-    // </div>
 
     <MDBContainer>
   <MDBCard style={{ width: "22rem", marginTop: "1rem" }}>
     <MDBCardBody>
       <MDBCardTitle 
-            className="text-center dark-text font-weight-bold text-uppercase" ><h5>{props.title}</h5></MDBCardTitle>
+            className="text-center blue-text font-weight-bold text-uppercase" ><h5>{props.title}</h5></MDBCardTitle>
+            <hr />
       <MDBCardTitle tag="h6" sub className="mb-2 text-muted">
         Author: {props.author}
       </MDBCardTitle>
@@ -26,11 +30,15 @@ function Projects(props) {
         Some quick example text to build on the panel title and make up
         the bulk of the panel's content.{" "}
       </MDBCardText>
-      <MDBBtn>Edit</MDBBtn>
+      <Link to={"/main/" + props.id}>
+      <MDBBtn className="blue-gradient" >Edit</MDBBtn>
+      </Link>
+      <MDBBtn onClick={props.onClick} className="peach-gradient float-right">Delete</MDBBtn>
+      {/* <MDBBtn onClick={()=>deleteBtn(props.id)} className="peach-gradient float-right">Delete</MDBBtn> */}
     </MDBCardBody>
   </MDBCard>
 </MDBContainer>
   );
 }
 
-export default Projects;
+export default withRouter(Projects);
--- controllers/lyricsController.js
+++ controllers/lyricsController.js
@@ -50,6 +50,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
+    console.log("made it here");
     db.Lyric
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
