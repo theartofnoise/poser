63d4d8d3dc1cfa9c7380c9290fcf65d81fa62ee5
Art G <theartofnoise@gmail.com>
2019-05-23 13:05:26 -0400

    ugh

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.Promise = Promise;
 if(process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI)
 } else {
-    mongoose.connect("mongodb://localhost:27017/pose", { useNewUrlParser: true });
+    mongoose.connect("mongodb://localhost/pose", { useNewUrlParser: true });
 }
 
 
